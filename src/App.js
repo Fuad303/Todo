19f8f016f3e08349e7b966bd35308ae09b50923f
Fuad303 <sadiqovfuad8@gmail.com>
2023-01-18 01:04:09 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AiOutlineOrderedList } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
 
-function App() {
+export default function App() {
+  const [todosText, setTodosText] = React.useState("")
+  const [todos, setTodos] = React.useState([])
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    setTodos([...todos, todosText])
+  }
+  const handleChange = (event) =>{
+    setTodosText(event.target.value)
+  }
+  const handleDelete = index =>{
+    setTodos(todos.filter((_, i) => i !== index))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='d-flex pt-4 justify-content-center align-items-center w-100 h-100'>
+      <div className='todo-list'>
+        <div className='d-flex align-items-center gap-2'>
+          <h3 className='text-center'>React Todo App</h3>
+          <AiOutlineOrderedList size={28}/>
+        </div>
+      <form onSubmit={handleSubmit}>
+        <input required placeholder='Enter something' className='border border-secondary rounded h-100 p-2' onChange={handleChange} type="text"/>
+        <button className='btn btn-success p-2'>Submit</button>
+        </form>
+        <ul className='list-none'>
+        { todos.map((item, index) => 
+         <li className='bg-light' key={index}><p className='text-left'>{index + 1}) {item}</p><TiDeleteOutline className='deleteIcon' size={28} onClick={() => handleDelete(index)}/></li>
+        )}
+        </ul>
+        </div>
     </div>
-  );
+  )
 }
-
-export default App;
